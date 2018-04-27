ac41fc255438a289fa27670e0e1172781e4b281a
Jane Costa <yberdysh@gmail.com>
2018-04-27 13:12:40 -0400

    merging work

---
+++ app/components/AddButton.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  AppRegistry,
+  TouchableHighlight
+} from "react-native";
+const styles = require("../style.js");
+const constants = styles.constants;
+
+export default class AddButton extends Component {
+  render() {
+    return (
+      <View style={styles.action}>
+        <TouchableHighlight
+          underlayColor="#24ce84"
+          onPress={this.props.onPress}
+        >
+          <Text style={styles.actionText}>{this.props.title}</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
---
+++ app/style.js
@@ -0,0 +1,65 @@
+'use strict'
+let React = require('react-native')
+let {StyleSheet} = React;
+const constants = {
+  actionColor: '#3cb371'
+}
+
+module.exports = StyleSheet.create({
+  container: {
+    backgroundColor: '#f2f2f2',
+    flex: 1
+  },
+  li: {
+    backgroundColor: '#fff',
+    borderBottomColor: '#eee',
+    borderWidth: 'transparent',
+    borderWidth: 1,
+    paddingLeft: 16,
+    paddingTop: 14,
+    paddingBottom: 16
+  },
+  liContainer: {
+    flex: 2
+  },
+  liText: {
+    color: '#333',
+    fontSize: 16
+  },
+  navbar: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderBottomColor: '#eee',
+    borderColor: 'transparent',
+    borderWidth: 1,
+    justifyContent: 'center',
+    height: 44,
+    flexDirection: 'row'
+  },
+  navbarTitle: {
+    color: '#444',
+    fontSize: 16,
+    fontWeight: "500"
+  },
+  toolbar: {
+    backgroundColor: '#fff',
+    height: 22
+  },
+  center: {
+    textAlign: 'center'
+  },
+  actionText: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center'
+  },
+  action: {
+    backgroundColor: constants.actionColor,
+    borderColor: 'transparent',
+    borderWidth: 1,
+    paddingLeft: 16,
+    paddingLeft: 14,
+    paddingBottom: 16
+  }
+})
+
