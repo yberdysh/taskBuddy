4c9cab25bae6caecb46660b8bc517378c8fc0fdf
Mahiyaa <mmahia1121@gmail.com>
2018-04-29 17:56:09 -0400

    edited sign up

--- app/components/SignUp.js
+++ app/components/SignUp.js
@@ -12,18 +12,7 @@ export default class SignUp extends Component {
       password: ""
     };
   }
-
-  // async signup(email, pass) {
-  //   try {
-  //     await firebase.auth().createUserWithEmailAndPassword(email, pass);
-
-  //     console.log("Account created");
-
-  //     // Navigate to the Home page, the user is auto logged in
-  //   } catch (error) {
-  //     console.log(error.toString());
-  //   }
-  // }
+  
   render() {
     return (
       <View style={styles.container}>
